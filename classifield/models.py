daaf105dd00d0d8fe0587dbd26deb51ca96c37a0
latsha <latipovsharif@gmail.com>
2019-01-15 15:30:53 +0500

    define initial models

--- classifield/models.py
+++ classifield/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class City(models.Model):
+    """Город к которому объявления относится"""
+    # Могут быть города с одинаковымы назаваниями
+    # можно сделать комбинированный ключ из 
+    # страны, области и города
+    name = models.Model(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+
+
+class Classfield(models.Model):
+    """Объявление"""
+    header = models.CharField()
+    body = models.TextField()
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
+
+
+class Counter(models.Model):
+    """Счетчик просмотра объявлений"""
+    classfield = models.ForeignKey(Classfield, on_delete=models.CASCADE)
+    value = models.PositiveIntegerField()
+
+    class Meta:
+        verbose_name = 'Счетчик'
+        verbose_name_plural = 'Счетчики'
